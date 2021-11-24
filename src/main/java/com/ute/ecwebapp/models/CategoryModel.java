ce8284cc070f2b08cb28307b9e37a473f16891b7
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2021-11-24 21:58:49 +0700

    bo sung noi dung x7 nav.jsp main.tag fix loi

--- src/main/java/com/ute/ecwebapp/models/CategoryModel.java
+++ src/main/java/com/ute/ecwebapp/models/CategoryModel.java
@@ -19,6 +19,8 @@ public class CategoryModel {
 //                new Category(5, "Loa")));
 //
 //    }
+
+
     public static List<Category> findAll() {
 
         final String query = "select * from categories ";
